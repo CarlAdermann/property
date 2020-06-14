628252a9b08ced5d78d4d54e16d1f686b9554d17
Carl Adermann <carl.adermann@redmarker.ai>
2020-06-14 13:50:05 +1000

    Adding migrations

---
+++ database/migrations/2020_06_12_042312_create_properties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('guid', 36)->index()->nullable();
+            $table->string('suburb', 50)->index();
+            $table->string('state', 20)->index();
+            $table->string('country', 30)->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
